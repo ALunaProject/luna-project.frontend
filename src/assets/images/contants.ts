932dd22ac35bbdf76ab54aca102f685235342e30
lyanbrito <lyanbrito613@gmail.com>
2026-09-05 18:58:39 -0300

    assets: added placeholder default images

---
+++ src/assets/images/contants.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_AVATAR = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwIoKBtBMERe4gtT1eueK4-5flQr0m6jH85-RXRDVHQQ&s";
+export const DEFAULT_BANNER = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTBGDdRCjkglWm9ueAyne1mhAiDPNMfqKVKATvnHK-lA&s";
