a6beb0cd1ab0d7c29875c2057fb0f50b8bcc4813
lyanbrito <lyanbrito613@gmail.com>
2026-09-05 18:57:19 -0300

    feat: created api integration

---
+++ src/api/api.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+function getBaseUrl() {
+    // url relativa (client)
+    if (typeof window !== "undefined") {
+        return process.env.NEXT_PUBLIC_API_URL ?? "/mocks";
+    }
+
+    // node (server) não sabe resolver caminho relativo
+    return process.env.NEXT_PUBLIC_API_URL
+        ?? `http://localhost:${process.env.PORT ?? 3000}/mocks`;
+}
+
+export const api = axios.create({
+    baseURL: getBaseUrl(),
+});
